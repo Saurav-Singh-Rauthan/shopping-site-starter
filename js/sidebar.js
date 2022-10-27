743419a3d154b7dd673196c1dcfa3eb6d44cb687
Saurav-Singh-Rauthan <sauravsinghrauthan@gmail.com>
2022-10-27 09:40:38 +0530

    stater files

---
+++ js/sidebar.js
@@ -0,0 +1,25 @@
+var modal = document.querySelector(".backdrop");
+var sidebar = document.querySelector(".sidebar");
+var btn = document.querySelector(".nav-switch");
+var links = document.querySelectorAll(".links");
+
+//Fill int he displayModal and HideModal functions the methods are already linked to buttons
+
+const displayModal = () => {};
+
+const hideModal = () => {};
+
+btn.addEventListener("click", () => {
+  displayModal();
+});
+
+modal.addEventListener("click", () => {
+  hideModal();
+});
+
+for (let index = 0; index < links.length; index++) {
+  const element = links[index];
+  element.addEventListener("click", () => {
+    hideModal();
+  });
+}
